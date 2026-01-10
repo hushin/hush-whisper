a22ada3db96cebd509c452cc80b7d13594e8a5a2
hushin <ngtv.hsn@gmail.com>
2026-01-10 11:17:41 +0900

    fix: 不要ファイルを削除

--- src/lib/components/categories/RecordingAndResult.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import RecordingSettings from "./RecordingSettings.svelte";
-  import StatusAndResult from "$lib/components/StatusAndResult.svelte";
-</script>
-
-<RecordingSettings />
-<StatusAndResult />
--- src/lib/components/categories/ResultViewer.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import StatusAndResult from "$lib/components/StatusAndResult.svelte";
-</script>
-
-<StatusAndResult />
